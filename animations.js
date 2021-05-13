051db3cb0e16c89c9ec614de3d1e44b546e1a2d5
Ken Sparks <kensparks.dev@gmail.com>
2021-05-13 10:12:03 -0400

    updated animation. X and Learn More buttons display changes

--- animations.js
+++ animations.js
@@ -6,22 +6,23 @@ learnMoreBtnOne.addEventListener('click', expandCardOne);
 closeBtnOne.addEventListener('click', closeCardOne);
 
 function expandCardOne() {
-  if (window.innerWidth >= 1024) {
-    sedansCard.style.transform = 'translateX(100px)';
-    sedansCard.style.width = '1000px';
-    sedansCard.style.height = '600px';
-    sedansCard.style.marginLeft = '620px';
-    sedansCard.style.zIndex = '2';
-    sedansCard.style.transition = '3s';
-    sedansCard.style.borderRadius = '15px';
-    closeBtnOne.style.display = 'block';
-    closeBtnOne.style.borderRadius = '50%';
-    closeBtnOne.style.transitionAll = '2s';
-    closeBtnOne.style.top = '580px';
-    closeBtnOne.style.left = '980px';
-    closeBtnOne.style.opacity = '100%'
-    closeBtnOne.style.transform = 'translateX(15px)'
-  }
+	if (window.innerWidth >= 1024) {
+		sedansCard.style.transform = 'translateX(100px)';
+		sedansCard.style.width = '1000px';
+		sedansCard.style.height = '600px';
+		sedansCard.style.marginLeft = '620px';
+		sedansCard.style.zIndex = '2';
+		sedansCard.style.transition = '1s';
+		sedansCard.style.borderRadius = '15px';
+		closeBtnOne.style.display = 'block';
+		closeBtnOne.style.borderRadius = '50%';
+		closeBtnOne.style.transitionAll = '2s';
+		closeBtnOne.style.top = '580px';
+		closeBtnOne.style.left = '980px';
+		closeBtnOne.style.opacity = '100%';
+    closeBtnOne.style.transform = 'translateX(15px)';
+    learnMoreBtnOne.style.display = 'none';
+	}
 }
 
 function closeCardOne() {
@@ -30,9 +31,10 @@ function closeCardOne() {
 	sedansCard.style.height = 'auto';
 	sedansCard.style.marginLeft = '0px';
 	sedansCard.style.zIndex = '1';
-	sedansCard.style.transition = '3s';
-  sedansCard.style.borderRadius = '15px 0px 0px 15px';
-  closeBtnOne.style.display = 'none';
+	sedansCard.style.transition = '1s';
+	sedansCard.style.borderRadius = '15px 0px 0px 15px';
+	closeBtnOne.style.display = 'none';
+	learnMoreBtnOne.style.display = 'block';
 }
 
 //column Two btn functions
@@ -41,24 +43,24 @@ let suvCard = document.querySelector('.column-two');
 let closeBtnTwo = document.querySelector('.close-btn-two');
 learnMoreBtnTwo.addEventListener('click', expandCardTwo);
 closeBtnTwo.addEventListener('click', closeCardTwo);
-
 function expandCardTwo() {
-  if (window.innerWidth >= 1024) {
-    suvCard.style.transform = 'translateX(0px)';
-    suvCard.style.width = '1000px';
-    suvCard.style.height = '600px';
-    suvCard.style.marginLeft = '0px';
-    suvCard.style.zIndex = '2';
-    suvCard.style.transition = '3s';
-    suvCard.style.borderRadius = '15px';
-    closeBtnTwo.style.display = 'block';
-    closeBtnTwo.style.borderRadius = '50%';
-    closeBtnTwo.style.transitionAll = '2s';
-    closeBtnTwo.style.top = '580px';
-    closeBtnTwo.style.left = '980px';
-    closeBtnTwo.style.opacity = '100%'
-    closeBtnTwo.style.transform = 'translateX(15px)'
-  }
+	if (window.innerWidth >= 1024) {
+		suvCard.style.transform = 'translateX(0px)';
+		suvCard.style.width = '1000px';
+		suvCard.style.height = '600px';
+		suvCard.style.marginLeft = '0px';
+		suvCard.style.zIndex = '2';
+		suvCard.style.transition = '1s';
+		suvCard.style.borderRadius = '15px';
+		closeBtnTwo.style.display = 'block';
+		closeBtnTwo.style.borderRadius = '50%';
+		closeBtnTwo.style.transitionAll = '2s';
+		closeBtnTwo.style.top = '580px';
+		closeBtnTwo.style.left = '980px';
+		closeBtnTwo.style.opacity = '100%';
+		closeBtnTwo.style.transform = 'translateX(15px)';
+		learnMoreBtnTwo.style.display = 'none';
+	}
 }
 
 function closeCardTwo() {
@@ -67,9 +69,10 @@ function closeCardTwo() {
 	suvCard.style.height = 'auto';
 	suvCard.style.margin = '0px';
 	suvCard.style.zIndex = '1';
-	suvCard.style.transition = '3s';
-  suvCard.style.borderRadius = '0px 0px 0px 0px';
-  closeBtnTwo.style.display = 'none';
+	suvCard.style.transition = '1s';
+	suvCard.style.borderRadius = '0px 0px 0px 0px';
+	closeBtnTwo.style.display = 'none';
+	learnMoreBtnTwo.style.display = 'block';
 }
 
 //column three btn functions
@@ -80,22 +83,23 @@ learnMoreBtnThree.addEventListener('click', expandCardThree);
 closeBtnThree.addEventListener('click', closeCardThree);
 
 function expandCardThree() {
-  if (window.innerWidth >= 1024) {
-	LuxuryCard.style.transform = 'translateX(100px)';
-	LuxuryCard.style.width = '1000px';
-	LuxuryCard.style.height = '600px';
-	LuxuryCard.style.marginLeft = '-620px';
-	LuxuryCard.style.zIndex = '2';
-	LuxuryCard.style.transition = '3s';
-	LuxuryCard.style.borderRadius = '15px';
-	closeBtnThree.style.display = 'block';
-	closeBtnThree.style.borderRadius = '50%';
-	closeBtnThree.style.transitionAll = '2s';
-	closeBtnThree.style.top = '580px';
-  closeBtnThree.style.left = '980px';
-  closeBtnThree.style.opacity = '100%'
-    closeBtnThree.style.transform = 'translateX(15px)'
-  }
+	if (window.innerWidth >= 1024) {
+		LuxuryCard.style.transform = 'translateX(100px)';
+		LuxuryCard.style.width = '1000px';
+		LuxuryCard.style.height = '600px';
+		LuxuryCard.style.marginLeft = '-620px';
+		LuxuryCard.style.zIndex = '2';
+		LuxuryCard.style.transition = '1s';
+		LuxuryCard.style.borderRadius = '15px';
+		closeBtnThree.style.display = 'block';
+		closeBtnThree.style.borderRadius = '50%';
+		closeBtnThree.style.transitionAll = '2s';
+		closeBtnThree.style.top = '580px';
+		closeBtnThree.style.left = '980px';
+		closeBtnThree.style.opacity = '100%';
+		closeBtnThree.style.transform = 'translateX(15px)';
+		learnMoreBtnThree.style.display = 'none';
+	}
 }
 
 function closeCardThree() {
@@ -104,7 +108,8 @@ function closeCardThree() {
 	LuxuryCard.style.height = 'auto';
 	LuxuryCard.style.marginLeft = '0px';
 	LuxuryCard.style.zIndex = '1';
-	LuxuryCard.style.transition = '3s';
-  LuxuryCard.style.borderRadius = '0px 15px 15px 0px';
-  closeBtnThree.style.display = 'none';
+	LuxuryCard.style.transition = '1s';
+	LuxuryCard.style.borderRadius = '0px 15px 15px 0px';
+	closeBtnThree.style.display = 'none';
+	learnMoreBtnThree.style.display = 'block';
 }
